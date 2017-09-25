d352cc367772e96089188c24f6b36e99d37228a9
ASUS <ASUS>
2017-09-24 23:34:18 -0700

    copy three method from part1

--- StringSecondAssignment/Part3.java
+++ StringSecondAssignment/Part3.java
@@ -2,5 +2,70 @@
 
 public class Part3
 {
+    int findStopCodon(String DNA , int startIndex , String stopCodon){
+    
+               int endIndex = startIndex;
+               while(endIndex != -1){
+           
+                    endIndex = DNA.indexOf(stopCodon ,endIndex + 1);
+              
+                if(endIndex != -1 && (endIndex - startIndex) % 3 ==0){
+            
+                       return endIndex;
+            
+               }
+            
+            }
+        
+          return DNA.length();
+    
+       }
+        
+    
+    String findGene(String DNA){
+      
+        int startIndex = DNA.indexOf("ATG");
+        
+        if(startIndex == -1)
+             return "";
+        
+        int index = findStopCodon(DNA , startIndex , "TAA");
+        if(index == DNA.length()){
+             index = findStopCodon(DNA , startIndex , "TAG");
+            }
+        
+        if(index == DNA.length()) 
+             return "";
+        else
+             return DNA.substring(startIndex , index + 3);
+    
+       }
+    
+    void findAllGenes(String DNA){
+       String gene = "";
+       int count = 1;
+       int startIndex = 0;
+       int index = -1;
+       do{
+          startIndex = DNA.indexOf("ATG",index + 1);
+         if(startIndex == -1)
+             break;
+           
+         index = findStopCodon(DNA , startIndex ,"TAA");
+         if(index == DNA.length()){
+            
+             index = findStopCodon(DNA , startIndex , "TAG");
+            }
+         
+         if(index == DNA.length())
+             break;
+         gene = DNA.substring(startIndex , index + 3);
+         System.out.println("Gene " + ":" + gene);
+          
+        }while(gene != "");
+    
+      }
+    
+    
     
 }
