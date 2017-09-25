3badfe2c2e0e7a9aa2684b5a6cb7d73e55003ba0
ASUS <ASUS>
2017-09-24 23:25:50 -0700

    Add howMany method to test that how many times a dna string is occure in another string dna and add testHowMany to test the method.

--- StringSecondAssignment/Part2.java
+++ StringSecondAssignment/Part2.java
@@ -1,6 +1,32 @@
 
 public class Part2
 {
-
+    int howMany(String stringa , String stringb){
+ 
+            int count = 0;
+            int index = -1;
+   
+            do{
+                index = stringb.indexOf(stringa , index + stringa.length());
+                if(index != -1)
+                      count = count + 1;
+      
+             }while(index != -1);
     
+           return count;
+
+       }
+ 
+    void testHowmany(){
+      
+        String stringb = "ATAAAA";
+        String stringa = "AA";
+        System.out.println(howMany(stringa , stringb));
+      
+        stringb = "ATGAACGAATTGAATC";
+        stringa = "GAA";
+        System.out.println(howMany(stringa , stringb));
+    
+    
+       }   
 }
