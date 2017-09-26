6ac39685f51d75e0a70c8a0f82dc9767df701069
ASUS <ASUS>
2017-09-26 12:23:58 -0700

    add testing method to test the twoOccurrence method

--- StringFirstAssignment/Part3.java
+++ StringFirstAssignment/Part3.java
@@ -2,7 +2,7 @@
 
 public class Part3
 {
-  public boolean twoOccurrence(String stringa , String stringb) {
+    public boolean twoOccurrence(String stringa , String stringb) {
     
         int currIndex = 0 , count = 0;
         int l= stringa.length();
@@ -23,8 +23,57 @@ public class Part3
          
             }
        
-        }  
+       }  
     
+    void testing(){
+           
+        String stringa = "by";
+        String stringb = "A shorty by abby long";
+        System.out.println(stringa);
+        System.out.println(stringb);
+        if(twoOccurrence(stringa , stringb)){
+           System.out.println(twoOccurrence(stringa , stringb));
+        
+        }
+        else
+           System.out.println(twoOccurrence(stringa , stringb));
+        
+        stringa = "abc";
+        stringb = "abcdefghab";
+        System.out.println(stringa);
+        System.out.println(stringb);
+        if(twoOccurrence(stringa , stringb)){
+           System.out.println(twoOccurrence(stringa , stringb));
+        
+        }
+        else
+           System.out.println(twoOccurrence(stringa , stringb));
+        
+        stringa = "na";
+        stringb = "banana";
+        System.out.println(stringa);
+        System.out.println(stringb);
+        if(twoOccurrence(stringa , stringb)){
+           System.out.println(twoOccurrence(stringa , stringb));
+        
+        }
+        else
+           System.out.println(twoOccurrence(stringa , stringb));
+    
+        stringa = "pp";
+        stringb = "apple";
+        System.out.println(stringa);
+        System.out.println(stringb);
+        if(twoOccurrence(stringa , stringb)){
+           System.out.println(twoOccurrence(stringa , stringb));
+        
+        }
+        else
+           System.out.println(twoOccurrence(stringa , stringb));
+    
+           
+           
+    }
         
   
     
