0bd5f8d93210d4de408c77a4267d6a2bdffeb354
ASUS <ASUS>
2017-09-26 12:10:25 -0700

    add twoOccurrence method to find that a string is appear in twice in another string.

--- StringFirstAssignment/Part3.java
+++ StringFirstAssignment/Part3.java
@@ -2,5 +2,37 @@
 
 public class Part3
 {
-   
+  public boolean twoOccurrence(String stringa , String stringb) {
+    
+        int currIndex = 0 , count = 0;
+        int l= stringa.length();
+        currIndex = stringb.indexOf(stringa , currIndex );
+         while(true){
+    
+             if(currIndex != -1){
+                 count = count + 1;
+             if(count == 2)
+                 return true;
+        
+        
+            }
+            else
+                return false;  
+            
+                currIndex = stringb.indexOf(stringa , currIndex + 1);
+         
+            }
+       
+        }  
+    
+        
+  
+    
+    
+    
+    
+    
+    
+    
+    
 }
