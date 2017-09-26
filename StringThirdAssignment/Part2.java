65f016f21011c613d04d3bc85ad88ad4fb412035
ASUS <ASUS>
2017-09-26 09:49:55 -0700

    add cgRatio method to count the number of G's and C' in a DNA sequence and add testcgRatio to test the method.

--- StringThirdAssignment/Part2.java
+++ StringThirdAssignment/Part2.java
@@ -3,4 +3,22 @@
 public class Part2
 {
 
+   float cgRatio(String dna){
+         int count =0;
+         for(int i = 0; i != dna.length(); i++){
+            if(dna.charAt(i) == 'G' || dna.charAt(i) == 'C')
+               count = count + 1;
+            
+            }
+    
+            return (float)count/dna.length();
+     }
+    
+   void testcgRatio(){
+    
+      String dna = "ATGCCATAG";
+      System.out.println(cgRatio(dna));
+    
+    
+     }
 }
