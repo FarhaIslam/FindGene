eb282743438be2de3c0a486b2bcdc0640889e5d6
ASUS <ASUS>
2017-09-26 22:44:21 -0700

    add the link in URLResource

--- StringFirstAssignment/Part4.java
+++ StringFirstAssignment/Part4.java
@@ -1,6 +1,11 @@
-
+import edu.duke.*;
 
 public class Part4
 {
-
+ public void checkYoutube(){
+     URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+    
+    
+    
+    }
 }
