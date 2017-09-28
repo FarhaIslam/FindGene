1309acab465c204def01cb46e4e7ea96962d45ea
ASUS <ASUS>
2017-09-27 22:32:53 -0700

    add  checkYoutube method to print out all the youtube links present in the given url link.

--- StringFirstAssignment/Part4.java
+++ StringFirstAssignment/Part4.java
@@ -3,9 +3,22 @@ import edu.duke.*;
 public class Part4
 {
  public void checkYoutube(){
-     URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+    
+    URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+    for(String s : res.words()){
+    
+     s = s.toLowerCase();
+     int index = s.indexOf("youtube.com");
+     
+     if(index != -1){
+        
+        index = s.indexOf("\"");
+        int endIndex = s.indexOf("\"" , index + 1);
+        System.out.println(s.substring(index + 1 , endIndex));
+           }
+    
+       }
     
     
-    
-    }
+   }
 }
